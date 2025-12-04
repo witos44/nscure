71ec32903f7f020b9b5a329190fbf866b1dd0f4d
witos44 <wito.forbes@gmail.com>
2025-12-04 12:43:16 +0700

    dropdown menu edit

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -2,12 +2,11 @@
 
 import Link from "next/link";
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuContent,
-} from "@/components/ui/navigation-menu";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
 
 export default function MainNav() {
   return (
@@ -18,96 +17,90 @@ export default function MainNav() {
           SecureRemote
         </Link>
 
-        <NavigationMenu>
-          <NavigationMenuList className="space-x-4">
+        <div className="flex items-center space-x-4">
+          {/* Security Tools */}
+          <Popover>
+            <PopoverTrigger className="flex items-center gap-1 text-sm font-medium hover:underline">
+              Security Tools <ChevronDown size={14} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[350px] p-4 bg-white" align="start">
+              <MenuItem href="/vpn">Best VPNs</MenuItem>
+              <MenuItem href="/password-managers">Password Managers</MenuItem>
+              <MenuItem href="/antivirus">Antivirus</MenuItem>
+              <MenuItem href="/secure-cloud">Secure Cloud Storage</MenuItem>
+              <MenuItem href="/hardware-keys">2FA Hardware Keys</MenuItem>
+              <MenuItem href="/secure-email">Secure Email Providers</MenuItem>
+            </PopoverContent>
+          </Popover>
 
-            {/* Security Tools */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Security Tools</NavigationMenuTrigger>
-              <NavigationMenuContent className="grid w-[350px] gap-1 p-4">
-                <MenuItem href="/vpn">Best VPNs</MenuItem>
-                <MenuItem href="/password-managers">Password Managers</MenuItem>
-                <MenuItem href="/antivirus">Antivirus</MenuItem>
-                <MenuItem href="/secure-cloud">Secure Cloud Storage</MenuItem>
-                <MenuItem href="/hardware-keys">2FA Hardware Keys</MenuItem>
-                <MenuItem href="/secure-email">Secure Email Providers</MenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          {/* Remote Work */}
+          <Popover>
+            <PopoverTrigger className="flex items-center gap-1 text-sm font-medium hover:underline">
+              Work From Anywhere <ChevronDown size={14} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[350px] p-4 bg-white" align="start">
+              <MenuItem href="/remote-jobs">Remote Job Boards</MenuItem>
+              <MenuItem href="/remote-tools">Team Collaboration Tools</MenuItem>
+              <MenuItem href="/best-laptops">Best Laptops for Remote Work</MenuItem>
+              <MenuItem href="/best-headsets">Best Headsets & Webcams</MenuItem>
+            </PopoverContent>
+          </Popover>
 
-            {/* Remote Work */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Work From Anywhere</NavigationMenuTrigger>
-              <NavigationMenuContent className="grid w-[350px] gap-1 p-4">
-                <MenuItem href="/remote-jobs">Remote Job Boards</MenuItem>
-                <MenuItem href="/remote-tools">Team Collaboration Tools</MenuItem>
-                <MenuItem href="/best-laptops">Best Laptops for Remote Work</MenuItem>
-                <MenuItem href="/best-headsets">Best Headsets & Webcams</MenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          {/* Deals */}
+          <Popover>
+            <PopoverTrigger className="flex items-center gap-1 text-sm font-medium hover:underline">
+              Deals <ChevronDown size={14} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[300px] p-4 bg-white" align="start">
+              <MenuItem href="/deals/vpn">VPN Deals</MenuItem>
+              <MenuItem href="/deals/software">Software Discounts</MenuItem>
+              <MenuItem href="/deals/gear">Remote Gear Deals</MenuItem>
+            </PopoverContent>
+          </Popover>
 
-            {/* Deals */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Deals</NavigationMenuTrigger>
-              <NavigationMenuContent className="grid w-[300px] gap-1 p-4">
-                <MenuItem href="/deals/vpn">VPN Deals</MenuItem>
-                <MenuItem href="/deals/software">Software Discounts</MenuItem>
-                <MenuItem href="/deals/gear">Remote Gear Deals</MenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          {/* Guides */}
+          <Popover>
+            <PopoverTrigger className="flex items-center gap-1 text-sm font-medium hover:underline">
+              Guides <ChevronDown size={14} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[350px] p-4 bg-white" align="start">
+              <MenuItem href="/guides/cybersecurity-basics">Cybersecurity Basics</MenuItem>
+              <MenuItem href="/guides/remote-work-starter">Remote Work Starter Kit</MenuItem>
+              <MenuItem href="/guides/secure-setup">Secure Remote Workspace</MenuItem>
+              <MenuItem href="/guides/privacy">Privacy Essentials</MenuItem>
+            </PopoverContent>
+          </Popover>
 
-            {/* Guides */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Guides</NavigationMenuTrigger>
-              <NavigationMenuContent className="grid w-[350px] gap-1 p-4">
-                <MenuItem href="/guides/cybersecurity-basics">Cybersecurity Basics</MenuItem>
-                <MenuItem href="/guides/remote-work-starter">Remote Work Starter Kit</MenuItem>
-                <MenuItem href="/guides/secure-setup">Secure Remote Workspace</MenuItem>
-                <MenuItem href="/guides/privacy">Privacy Essentials</MenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          {/* Reviews */}
+          <Popover>
+            <PopoverTrigger className="flex items-center gap-1 text-sm font-medium hover:underline">
+              Reviews <ChevronDown size={14} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[350px] p-4 bg-white" align="start">
+              <MenuItem href="/reviews/software">Software Reviews</MenuItem>
+              <MenuItem href="/reviews/hardware">Hardware Reviews</MenuItem>
+              <MenuItem href="/reviews/platforms">Remote Job Platforms</MenuItem>
+            </PopoverContent>
+          </Popover>
 
-            {/* Reviews */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Reviews</NavigationMenuTrigger>
-              <NavigationMenuContent className="grid w-[350px] gap-1 p-4">
-                <MenuItem href="/reviews/software">Software Reviews</MenuItem>
-                <MenuItem href="/reviews/hardware">Hardware Reviews</MenuItem>
-                <MenuItem href="/reviews/platforms">Remote Job Platforms</MenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-
-            {/* Regular Links */}
-            <NavigationMenuItem>
-              <Link href="/resources" className="px-2 py-1 text-sm">
-                Resources
-              </Link>
-            </NavigationMenuItem>
-
-            <NavigationMenuItem>
-              <Link href="/blog" className="px-2 py-1 text-sm">
-                Blog
-              </Link>
-            </NavigationMenuItem>
-
-          </NavigationMenuList>
-        </NavigationMenu>
+          {/* Static Links */}
+          <Link href="/resources" className="text-sm font-medium hover:underline">
+            Resources
+          </Link>
+          <Link href="/blog" className="text-sm font-medium hover:underline">
+            Blog
+          </Link>
+        </div>
       </div>
     </nav>
   );
 }
 
-/* Small helper component */
-function MenuItem({
-  href,
-  children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
+function MenuItem({ href, children }: { href: string; children: React.ReactNode }) {
   return (
     <Link
       href={href}
-      className="rounded-md px-3 py-2 text-sm transition hover:bg-gray-100"
+      className="block rounded px-3 py-2 text-sm transition hover:bg-gray-100"
     >
       {children}
     </Link>
